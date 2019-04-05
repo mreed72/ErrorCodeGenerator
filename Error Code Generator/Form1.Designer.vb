0c878bad44a5f63541accab1512fea3595618163
Scott Reed <msrphoto@outlook.com>
2019-04-05 07:25:55 -0500

    0.0.4
    Minor Code Cleanup

--- Error Code Generator/Form1.Designer.vb
+++ Error Code Generator/Form1.Designer.vb
@@ -29,6 +29,7 @@ Partial Class Form1
         Me.lbx = New System.Windows.Forms.ListBox()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -36,9 +37,9 @@ Partial Class Form1
         '
         Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Panel1.Controls.Add(Me.txCodeDec)
-        Me.Panel1.Location = New System.Drawing.Point(89, 22)
+        Me.Panel1.Location = New System.Drawing.Point(12, 22)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(125, 33)
+        Me.Panel1.Size = New System.Drawing.Size(181, 33)
         Me.Panel1.TabIndex = 0
         '
         'txCodeDec
@@ -47,13 +48,13 @@ Partial Class Form1
         Me.txCodeDec.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txCodeDec.Location = New System.Drawing.Point(3, 3)
         Me.txCodeDec.Name = "txCodeDec"
-        Me.txCodeDec.Size = New System.Drawing.Size(115, 22)
+        Me.txCodeDec.Size = New System.Drawing.Size(171, 22)
         Me.txCodeDec.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(9, 31)
+        Me.Label1.Location = New System.Drawing.Point(9, 6)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(74, 13)
         Me.Label1.TabIndex = 1
@@ -64,9 +65,9 @@ Partial Class Form1
         Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Button1.Image = Global.Error_Code_Generator.My.Resources.Resources.x320733
-        Me.Button1.Location = New System.Drawing.Point(220, 12)
+        Me.Button1.Location = New System.Drawing.Point(199, 6)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(42, 43)
+        Me.Button1.Size = New System.Drawing.Size(63, 49)
         Me.Button1.TabIndex = 1
         Me.Button1.UseVisualStyleBackColor = False
         '
@@ -84,11 +85,11 @@ Partial Class Form1
         '
         Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(247, Byte), Integer), CType(CType(163, Byte), Integer), CType(CType(163, Byte), Integer))
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button2.Location = New System.Drawing.Point(220, 311)
+        Me.Button2.Image = Global.Error_Code_Generator.My.Resources.Resources._17041
+        Me.Button2.Location = New System.Drawing.Point(232, 311)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(42, 23)
+        Me.Button2.Size = New System.Drawing.Size(30, 23)
         Me.Button2.TabIndex = 3
-        Me.Button2.Text = "X"
         Me.Button2.UseVisualStyleBackColor = False
         '
         'Button3
@@ -96,16 +97,27 @@ Partial Class Form1
         Me.Button3.Image = Global.Error_Code_Generator.My.Resources.Resources.x1600571
         Me.Button3.Location = New System.Drawing.Point(12, 311)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(51, 23)
+        Me.Button3.Size = New System.Drawing.Size(31, 23)
         Me.Button3.TabIndex = 4
         Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Label2.Location = New System.Drawing.Point(53, 316)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(167, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "ELOG(""6X451TUK"", ex.message)"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(271, 347)
+        Me.ClientSize = New System.Drawing.Size(271, 340)
         Me.ControlBox = False
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.lbx)
@@ -130,4 +142,5 @@ Partial Class Form1
     Friend WithEvents lbx As ListBox
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
 End Class
--- Error Code Generator/My Project/AssemblyInfo.vb
+++ Error Code Generator/My Project/AssemblyInfo.vb
@@ -9,8 +9,8 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Error Code Generator")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyDescription("Generates Error Codes")>
+<Assembly: AssemblyCompany("SimonCoder Software")>
 <Assembly: AssemblyProduct("Error Code Generator")>
 <Assembly: AssemblyCopyright("Copyright ©  2018")>
 <Assembly: AssemblyTrademark("")>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.0.3")>
+<Assembly: AssemblyFileVersion("1.0.0.3")>
